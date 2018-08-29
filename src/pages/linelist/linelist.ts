55f6122f2471ff6d778575b218cd5f44e0696192
sachin <sachin.bonzer@gmail.com>
2018-08-29 15:09:59 +0530

    Changed text on line create screen

--- src/pages/linelist/linelist.ts
+++ src/pages/linelist/linelist.ts
@@ -502,6 +502,7 @@ export class LinePage {
                 text: emoji ?  emoji : this.txtIn.value,
                 text_with_links: emoji ?  emoji : this.txtIn.value,
                 user_id: this.user ? this.user.id : 0,
+                role_name: this.user.role_name,
                 images: emoji ? [] : images,
                 video_urls: params && params.video_urls ? params.video_urls : [],
                 undercover: (this.chatPrvd.getState() == 'area') ? false : this.isUndercover,

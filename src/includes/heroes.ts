c008a03716566e5036dce4f96f679d7f26d1851e
sachin <sachin.bonzer@gmail.com>
2018-10-31 15:58:53 +0530

    Added private network entry

--- src/includes/heroes.ts
+++ src/includes/heroes.ts
@@ -11,6 +11,11 @@ export const heroes: any = {
           imageUrl: 'assets/icon/netwrk-chat.svg'
         },
         {
+           name: 'Private network',
+           description: 'Name and describe the kind of network it is.',
+           imageUrl: 'assets/icon/netwrk-chat.svg'
+        },
+        {
           name: 'Temporary gathering',
           description: 'Name your gathering whatever you want.',
           imageUrl: 'assets/icon/netwrk-chat.svg'
@@ -23,6 +28,11 @@ export const heroes: any = {
           imageUrl: 'assets/icon/netwrk-chat.svg'
       },
       {
+          name: 'Private network',
+          description: 'Name and describe the kind of network it is.',
+          imageUrl: 'assets/icon/netwrk-chat.svg'
+      },
+      {
           name: 'Temporary gathering',
           description: 'Name your gathering whatever you want.',
           imageUrl: 'assets/icon/netwrk-chat.svg'
@@ -32,6 +42,11 @@ export const heroes: any = {
       {
           name: 'Public service network',
           description: 'Trying to help or support an area? Is a government location in need of a line? Hang a public service line in that spot',
+          imageUrl: 'assets/icon/netwrk-chat.svg'
+      },
+      {
+          name: 'Private network',
+          description: 'Name and describe the kind of network it is.',
           imageUrl: 'assets/icon/netwrk-chat.svg'
       },
       {
